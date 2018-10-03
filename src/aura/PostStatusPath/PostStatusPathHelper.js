1f6aa5b09d8271fd3f7aea74bfb764f54debdff7
kperez <kperez@altimetrik.com>
2018-10-03 12:54:46 -0300

    Changing names of variables in controller javascript classes

--- src/aura/PostStatusPath/PostStatusPathHelper.js
+++ src/aura/PostStatusPath/PostStatusPathHelper.js
@@ -1,36 +1,36 @@
 ({
 	updatePath: function (component) {
 		var currentStatus = component.get("v.current");
-		var li = component.find(currentStatus);
+		var currentStatusHtmlComponent = component.find(currentStatus);
 		switch (currentStatus) {
 			case 'Draft':
-				this.updateCurrentItemCss(li);
+				this.updateCurrentItemCss(currentStatusHtmlComponent);
 				break;
 			case 'Under Review':
 				this.updatePassedItemCss(component, 'Under Review');
-				this.updateCurrentItemCss(li);
+				this.updateCurrentItemCss(currentStatusHtmlComponent);
 				break;
 			case 'Ready':
 				this.updatePassedItemCss(component, 'Ready');
-				this.updateCurrentItemCss(li);
+				this.updateCurrentItemCss(currentStatusHtmlComponent);
 				break;
 			case 'Published':
 				this.updatePassedItemCss(component, 'Published');
-				this.updateCurrentItemCss(li);
+				this.updateCurrentItemCss(currentStatusHtmlComponent);
 				break;
 		}
 	},
-	updateCurrentItemCss: function (li) {
-		$A.util.removeClass(li, 'slds-is-incomplete');
-		$A.util.addClass(li, 'slds-is-current');
-		$A.util.addClass(li, 'slds-is-active');
+	updateCurrentItemCss: function (currentStatusHtmlComponent) {
+		$A.util.removeClass(currentStatusHtmlComponent, 'slds-is-incomplete');
+		$A.util.addClass(currentStatusHtmlComponent, 'slds-is-current');
+		$A.util.addClass(currentStatusHtmlComponent, 'slds-is-active');
 	},
 	updatePassedItemCss: function (component, status) {
 		switch (status) {
 			case 'Under Review':
-				var li = component.find('Draft');
-				this.removeActiveAndCurrentClassPreviusItem(li);
-				this.completedItemCss(li);
+				var currentStatusHtmlComponent = component.find('Draft');
+				this.removeActiveAndCurrentClassPreviusItem(currentStatusHtmlComponent);
+				this.completedItemCss(currentStatusHtmlComponent);
 				break;
 			case 'Ready':
 				this.completedItemCss(component.find('Draft'));
@@ -44,12 +44,12 @@
 				break;
 		}
 	},
-	completedItemCss: function (li) {
-		$A.util.removeClass(li, 'slds-is-incomplete');
-		$A.util.addClass(li, 'slds-is-complete');
+	completedItemCss: function (completedStatusHtmlComponent) {
+		$A.util.removeClass(completedStatusHtmlComponent, 'slds-is-incomplete');
+		$A.util.addClass(completedStatusHtmlComponent, 'slds-is-complete');
 	},
-	removeActiveAndCurrentClassPreviusItem: function (li) {
-		$A.util.removeClass(li, 'slds-is-active');
-		$A.util.removeClass(li, 'slds-is-current');
+	removeActiveAndCurrentClassPreviusItem: function (previousStatusHtmlComponent) {
+		$A.util.removeClass(previousStatusHtmlComponent, 'slds-is-active');
+		$A.util.removeClass(previousStatusHtmlComponent, 'slds-is-current');
 	}
 })
